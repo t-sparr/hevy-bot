e8cc84dff35ea41620076c4cb3181fa656013828
2JucyBlue <timothysparr2022@gmail.com>
2025-05-01 17:59:39 -0600

    first commit

--- check_token.py
+++ check_token.py
@@ -1,8 +1,12 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_KEY = os.environ['API_KEY']
 AUTH_TOKEN = os.environ['AUTH_TOKEN']
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1367640664669556857/sDi-4OIfk1gvE9Tu8AsoXbK9GhbwD0BIBuAEGrPb_QYwZenAoqsAjfjuEi7pVc-MCTPo'
 
 HEADERS = {
     'x-api-key': API_KEY,
@@ -18,7 +22,15 @@ def check_token():
         print("✅ Token is valid")
     else:
         print(f"❌ Token check failed. Status: {res.status_code}")
+        send_discord_alert("hevy bot boken is expired")
         raise SystemExit("Token expired or invalid!")
+
+def send_discord_alert(message):
+    payload = {'content': message}
+    try:
+        requests.post(WEBHOOK_URL, json=payload)
+    except Exception as e:
+        print(f"⚠️ Failed to send Discord alert: {e}")
 
 if __name__ == '__main__':
     check_token()
